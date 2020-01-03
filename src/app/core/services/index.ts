fa654c4b5ce8017e8805326d7a4eb65874127f8e
Juan Antonio <juagisla@gmail.com>
2020-01-03 16:41:16 +0100

    starting angular-redux offices

--- src/app/core/services/index.ts
+++ src/app/core/services/index.ts
@@ -7,3 +7,4 @@ export * from './profiles.service';
 export * from './tags.service';
 export * from './user.service';
 export * from './hotels.service';
+export * from './offices.service';
---
+++ src/app/core/services/offices.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Office } from '../models/office.model';
+
+// @Injectable({
+// 	providedIn: 'root'
+// })
+
+@Injectable()
+export class OfficesService {
+
+	constructor(
+		private apiService: ApiService
+	) {}
+
+	/**
+	 * Get all offices from server
+	 */
+	getAll(): Observable<Office[]> {
+		return this.apiService.get('/offices/')
+		.pipe(map(data => {
+			return data;
+		}));
+	}
+
+	getOne(slug): Observable<Office> {
+		return this.apiService.get(`/offices/${slug}`)
+		.pipe(map(data => {
+			return data;
+		}));
+	}
+
+}
